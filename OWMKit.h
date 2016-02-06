60eb87f4997a6813cf77d71617d194a066f05885
Stephen Radford <steve228uk@gmail.com>
2016-02-06 16:56:00 +0000

    Initial commit

---
+++ OWMKit.h
@@ -0,0 +1,19 @@
+//
+//  OWMKit.h
+//  OWMKit
+//
+//  Created by Stephen Radford on 06/02/2016.
+//  Copyright © 2016 Cocoon Development Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for OWMKit.
+FOUNDATION_EXPORT double OWMKitVersionNumber;
+
+//! Project version string for OWMKit.
+FOUNDATION_EXPORT const unsigned char OWMKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OWMKit/PublicHeader.h>
+
+
